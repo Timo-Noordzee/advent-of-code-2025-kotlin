4be3dfe6c5fcd01ce8873d88a02e338231a40af6
Timo Noordzee <t.noordzee@fleetgo.com>
2025-12-03 21:11:59 +0100

    Day02 - cleanup

--- src/Day02.kt
+++ src/Day02.kt
@@ -10,12 +10,10 @@ fun main() {
 
     fun part2(input: List<String>) = solve(input, "^(.+)\\1+$".toRegex())
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 1227775554L)
     check(part2(testInput) == 4174379265L)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
